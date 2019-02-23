dd2c01ec4e88a8f89f9c688448e3e22fb03ca1ba
Michael <zwang20@knox.nsw.edu.au>
2019-02-23 22:00:19 +1100

    Minor changes

--- main.py
+++ main.py
@@ -24,34 +24,15 @@ YELLOW = (255, 255, 0); DARK_YELLOW = (128, 128, 0)
 # creat window
 display_width = 1280
 display_height = 800
-display = pygame.display.set_mode([display_width, display_height], pygame.NOFRAME)
+display = pygame.display.set_mode([display_width, display_height])
 
-# set caption
-pygame.display.set_caption('Chaos')
-
-# set icon
-pygame.display.set_icon(
-    pygame.image.load(os.path.join('assets', '32x32_project_nont.png'))
-)
-
-# Load images
-rifle_img = pygame.image.load(os.path.join('Assets', 'guns', 'gun_rifle.png'))
-sniper_rifle_img = pygame.image.load(os.path.join('Assets', 'guns', 'gun_sniper.png'))
-
-# load sounds
-pistol_sound = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'M1911.ogg'))
-rifle_sound = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'M16.ogg'))
-reload_sound = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'Reload.ogg'))
-
-# sound channels
-CHANNELS = 100
-pygame.mixer.set_num_channels(CHANNELS)
-
-
-class GameObj(pygame.sprite.Sprite):
-
-    family = pygame.sprite.RenderUpdates()
-
-    def __init__(self):
-        super().__init__()
-        GameObj.family.add(self)
+# Load Everything
+os.chdir('assets')
+directories = []
+for file in os.listdir():
+    if file[0] == '.':
+        continue
+    elif '.' not in file:
+        directories.append(file)
+    else:
+        print(file)

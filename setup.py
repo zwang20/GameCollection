a10aacf8205f96da778b1f043f31c0c84bb8eef0
Michael <zwang20@knox.nsw.edu.au>
2019-02-24 21:59:01 +1100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+py2app build script for MyApplication
+
+Usage:
+    python setup.py py2app
+"""
+from setuptools import setup
+setup(
+    app=["main.py"],
+    options={'py2app': {'argv_emulation': True, 'packages': ['pygame']}},
+    data_files=['sge.py', 'crazy_spin_pvc.py', 'crazy_spin_pvp.py', 'Assets'],
+    setup_requires=["py2app"]
+)

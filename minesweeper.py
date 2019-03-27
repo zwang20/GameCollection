1cf623e946dc6f46460e48de5df72fd226d06b5d
Michael <zwang20@knox.nsw.edu.au>
2019-03-27 12:42:41 +1100

    Mine Sweeper

--- minesweeper.py
+++ minesweeper.py
@@ -1,1 +1,20 @@
 from cge import *
+
+pygame.display.set_caption('Mine Sweeper')
+
+GameObj.family.empty()
+
+DISPLAY.fill(WHITE)
+
+class Cell(GameObj):
+    pass
+
+def main():
+    while True:
+        get_input()
+
+        
+
+        GameObj.family.draw(DISPLAY) # draw sprites
+        pygame.display.update()  # update
+        # This should be the last thing in the loop

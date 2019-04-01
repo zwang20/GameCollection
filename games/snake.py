fa70e75825f08ecb9b4140b0fe89acb9aed89d90
Michael <zwang20@knox.nsw.edu.au>
2019-04-01 11:52:56 +1100

    Snake bad

--- games/snake.py
+++ games/snake.py
@@ -4,34 +4,38 @@ pygame.display.set_caption('Snake')
 
 DISPLAY.fill(WHITE)
 
-class Cell(GameObj):
+# class Cell(GameObj):
+#
+#     family = pygame.sprite.Group()
+#
+#     WIDTH = 10
+#     HEIGHT = 10
+#
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#         self.status = 0 # 0 = nothing, 1 = snake, 2 = food
+#         self.image = pygame.Surface(WIDTH, HEIGHT)
+#         self.image.fill(WHITE)
+#         self.rect = self.image.get_rect()
+#         self.rect.topleft = (self.x, self.y)
+#         Cell.family.add(self)
+#
+#     def change_status(self, new_status):
+#         self.status = new_status
+#
+#     def update(self):
+#         if self.status == 0:
+#             self.image.fill(WHITE)
+#         if self.status == 1:
+#             self.image.fill(BLACK)
+#         if self.status == 2:
+#             self.image.fill(RED)
 
-    family = pygame.sprite.Group()
+# WIDTH = 1280
+# HEIGHT = 800
 
-    WIDTH = 10
-    HEIGHT = 10
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.status = 0 # 0 = nothing, 1 = snake, 2 = food
-        self.image = pygame.Surface(WIDTH, HEIGHT)
-        self.image.fill(WHITE)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (self.x, self.y)
-        Cell.family.add(self)
-
-    def change_status(self, new_status):
-        self.status = new_status
-
-    def update(self):
-        if self.status == 0:
-            self.image.fill(WHITE)
-        if self.status == 1:
-            self.image.fill(BLACK)
-        if self.status == 2:
-            self.image.fill(RED)
-
+# [40, 140, 240, 340, 440, 540, 640, 740, 840, 940, 1040, 1140]
 
 def main():
     while True:

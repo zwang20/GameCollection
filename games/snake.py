737e56dd296dfc0c13115f689f87b0fe41cefdf0
Michael <zwang20@knox.nsw.edu.au>
2019-04-02 13:37:04 +1100

    Nothing

--- games/snake.py
+++ games/snake.py
@@ -38,10 +38,24 @@ DISPLAY.fill(WHITE)
 # [40, 140, 240, 340, 440, 540, 640, 740, 840, 940, 1040, 1140]
 
 def main():
+
+    snake_head = [340, 0]
+    snake_pos = numpy.array([[40, 0], [140, 0], [240, 0], [340, 0]])
+    direction = 'right'
+
     while True:
+        clock.tick(2)
         get_input()
+
+        if direction == 'right':
+            snake_head[0] += 100
+
+        snake_pos = numpy.append(snake_pos, [snake_head], axis=0)
+        print(snake_pos)
 
 
         GameObj.family.draw(DISPLAY) # draw sprites
         pygame.display.update()  # update
         # This should be the last thing in the loop
+
+main()
